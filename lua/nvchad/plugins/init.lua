e37b6c0a24eea5aa84d68efc105a80301abb81af
Kai Harms <kai.harms@tuhh.de>
2025-01-05 18:54:20 +0100

    blub

--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -30,20 +30,10 @@ return {
 
   {
     "lukas-reineke/indent-blankline.nvim",
-    event = "User FilePost",
-    opts = {
-      indent = { char = "│", highlight = "IblChar" },
-      scope = { char = "│", highlight = "IblScopeChar" },
-    },
-    config = function(_, opts)
-      dofile(vim.g.base46_cache .. "blankline")
-
-      -- local hooks = require "ibl.hooks"
-      -- hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
-      require("ibl").setup(opts)
-
-      dofile(vim.g.base46_cache .. "blankline")
-    end,
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
   },
 
   -- file managing , picker etc
